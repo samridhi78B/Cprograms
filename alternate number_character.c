f7a23e5c37552f77639d0c33b7b667a987d46d82
Samridhi Gumber <samridhigumber05@gmail.com>
2025-03-26 11:04:47 +0530

    pattern printing

---
+++ alternate number_character.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int i, j,n;
+  char ch;
+  scanf("%d", &n);
+  for(i = 1; i <= n; i++){
+      ch = 'A';
+       for(ch = 'A'; ch < 'A' + i; ch++){
+           if( i % 2 == 0){
+        printf("%c ", ch);
+           }
+        else {
+           printf("%d ", ch-'A'+1); 
+        }
+       }
+      printf("\n"); 
+  }
+
+    return 0;
+}
