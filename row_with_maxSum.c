470d1f918377b977e190f72f205e56444d107082
Samridhi Gumber <samridhigumber05@gmail.com>
2025-04-18 10:50:14 +0530

    array in 2D

---
+++ row_with_maxSum.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <limits.h>
+int main(){
+    int r,c;
+    scanf("%d %d", &r, &c); 
+    int arr[r][c];
+    for(int i = 0; i<r; i++){
+        for(int j = 0; j < c; j++){
+            scanf("%d", &arr[i][j]);  
+        }                            
+    }  
+    int maxSum = INT_MIN;
+    int rowSum;
+    int maxRow = -1;
+    for(int i = 0; i<r; i++){
+     rowSum = 0;
+        for(int j = 0; j < c; j++){
+            rowSum += arr[i][j];
+        }
+    
+          if(rowSum > maxSum){
+              maxSum = rowSum;
+              maxRow = i;
+          } 
+    } 
+
+    printf("Max rowis: %d\nMax sumis: %d", maxRow, maxSum);
+   return 0;
+}
