723d30b36ff91b59680b0614a68d367e28643673
Samridhi Gumber <samridhigumber05@gmail.com>
2025-05-11 11:43:12 +0530

    strings

--- reverse_string.c
+++ reverse_string.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
 #include <string.h>
-int main()
-{
+int main(){
     char str[100];
     gets(str);
-   int len = strlen(str);
-    for(int i = 0; i< len/2; i++){
-        char temp = str[i];
-        str[i] = str[len-i-1];
-        str[len-i-1] = temp;
+    
+    //size
+    int size = 0;
+    int k = 0;
+    while(str[k] != '\0'){
+        size++;
+        k++;
     }
-    printf("reverse string: %s", str);
-
+    
+    //reverse logic
+    for(int i = 0, j= size-1; i<=j; i++, j--){
+       char temp = str[i];
+       str[i] = str[j];
+       str[j] = temp;
+    }
+    puts("The reverse string is : ");
+    puts(str);
     return 0;
 }
